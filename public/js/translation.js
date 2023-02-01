288c7fcb5f4d8bdcecd6f323c8ff2c77c86ea699
Mohammad Adiban <69257287+mohammad-adiban@users.noreply.github.com>
2023-02-01 12:12:16 +0100

    Add files via upload

---
+++ public/js/translation.js
@@ -0,0 +1,108 @@
+var l_Fa = {
+    'header' : {
+        'login' : 'ورود',
+        'signup' : 'ثبت نام',
+        'title' : 'شب',
+        'host' : 'میزبان شوید'
+    },
+    'index': {
+        'welcome' : 'اجاره روزانه ویلا',
+        'welcomeDesc' : 'اقامتگاه‌های شخصی مسافرتی',
+        'shabHelp' : 'شب چطور کمکم می کند؟',
+        'search' : 'جستجو',
+        'guestCount' : 'چند نفرید؟',
+        'retDate' : 'کی بازمیگردید؟',
+        'startDate' : ' کی می روید؟ ',
+        'destination' : 'کجا می روید؟',
+        'Email' : 'پست الکترونیکی'
+    },
+    'login' : {
+        'email': 'ایمیل',
+        'password': 'رمز عبور',
+        'passwordConfirm' : 'تایید رمز عبور',
+        'forgotPwd': 'رمزتان را فراموش کرده اید؟',
+        'rememberMe': 'من را به خاطر بسپار',
+        'mobile' : 'شماره تلفن همراه',
+
+        'loginGoogle': 'ورود از طریق حساب گوگل',
+        'loginBtn': 'ورود',
+        'or': 'یا',
+        'signUp': 'ثبت نام',
+        'dontHaveAccount': 'حساب کاربری ندارید؟',
+        'resetPwd':'تغییر رمز عبور',
+        'resetBtn':'ارسال لینک',
+        'resetEmail'  : 'ایمیل خود را وارد کنید'
+    },
+    'search': {
+        'title': 'کد اگهی، عنوان، نام مالک را جستجو کنید',
+        'date': 'تاریخ',
+        'checkIn' : 'زمان رفت',
+        'checkOut' : 'زمان برگشت',
+        'description' : '',
+        'perMonth' : 'ماهانه',
+        'currency' : 'هزار تومان',
+        'review' : 'دیدگاه',
+        'guests' : 'چند نفرید؟',
+        'maxGuests' : 'تعداد نفرات',
+        'villa' : 'خانه ویلایی',
+        'entireHome' : 'آپارتمان',
+        'privateRoom' : 'سوئیت',
+        'roomType' : 'نوع اتاق',
+        'priceRange' : 'محدوده قیمت',
+        'size' : 'اندازه',
+        'morePlaces' : 'انتخاب های بیشتر ',
+        'peopleAlsoStay' : 'کاربران این مکان ها را نیز انتخاب نموده اند',
+        'whereTo' : 'کجا می روید؟'
+    },
+    'signup' : {
+        'email': 'ایمیل (اختیاری)',
+        'password': 'رمز عبور',
+        'name': 'نام',
+        'lname': 'نام خانوادگی',
+        'bday': 'تاریخ تولد',
+        'month': 'ماه',
+        'year': 'سال',
+        'day': 'روز',
+        'or': 'یا',
+        'mobile' : 'شماره تلفن همراه',
+        'signupGoogle': 'ثبت نام از طریق حساب گوگل',
+        'signUpBtn': 'ثبت نام',
+        'alreadyMember': 'عضو هستید؟',
+        'policy': 'با عضویت قوانین را قبول می کنید',
+        'loginBtn': 'وارد شوید'
+    },
+    'company' : {
+        'about': 'درباره شب',
+        'terms': 'حقوق کاربران در شب',
+        'refund': 'قواعد شب درباره بازپرداخت وجوه',
+        'careers': 'همکاری با شب',
+        'policies': 'قوانین استفاده از شب',
+        'help' : 'نحوه کار شب',
+        'helpHost': 'چگونه میزبان شوم؟',
+        'helpGuest' : 'چگونه میهمان شوم؟',
+        'complaints': 'ثبت شکایات',
+        'helpTrust': 'چگونه اعتماد کنم؟'
+    },
+    'user' : {
+        'newHouseButton' : 'ایجاد آگهی جدید',
+        'myListings' : 'آگهی های من',
+        'myReserves' : 'رزرو های من'
+    },
+    'dashboard' : {
+        'addPic': 'افزودن تصویر پروفایل',
+        'viewProfile' : 'مشاهده پروفایل',
+        'cmpltProfile' : 'تکمیل پروفایل',
+        'notifs' : 'اطلاعیه ها',
+        'invFriends' : 'دعوت از دوستان',
+        'editProfile' : 'ویرایش پروفایل'
+    },
+    'house' : {
+        'newHouse' : 'ایجاد آگهی جدید',
+        'editHouse' : 'ویرایش آگهی'
+    }
+
+};
+
+var l_En = {
+
+}
